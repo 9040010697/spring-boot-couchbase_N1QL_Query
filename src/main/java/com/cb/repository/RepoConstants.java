7948427a2b92ce053fd6894d6fbeae9bdd2d2e20
9040010697 <52064780+9040010697@users.noreply.github.com>
2020-05-26 08:57:23 +0530

    Update RepoConstants.java

--- src/main/java/com/cb/repository/RepoConstants.java
+++ src/main/java/com/cb/repository/RepoConstants.java
@@ -5,10 +5,10 @@ public class RepoConstants {
           + "WHERE ANY mlcCard IN mlcCards SATISFIES mlcCard.mlcCardNo = $1 END";
 
   public static final String PHONE_NUMBER_SEARCH_QUERY
-          = "#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND phoneNumber = $1";
+          = "#{#n1ql.selectEntity} WHERE phoneNumber = $1";
 
 
   public static final String MLC_CARD_PHONE_NUMBER_SEARCH_QUERY =
-          "#{#n1ql.selectEntity} WHERE #{#n1ql.filter} AND phoneNumber "
+          "#{#n1ql.selectEntity} WHERE phoneNumber "
                   + "= $2 AND ANY mlcCard IN mlcCards SATISFIES mlcCard.mlcCardNo = $1 END";
 }
